489d1c1a4e6f632494d31888798e2780ca3d3233
Desk1 <agv.ispc@gmail.com>
2023-10-18 14:30:01 -0300

    new file:   app/posts/[id]/comments/loading.jsx modified:   app/posts/[id]/comments/page.jsx

--- app/posts/[id]/comments/page.jsx
+++ app/posts/[id]/comments/page.jsx
@@ -1,6 +1,8 @@
 import { LikeButton } from '../../LikeButton'
 
-const fetchCommnets = (id) => {
+const fetchCommnets = async (id) => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: {
       revalidate: 60
@@ -12,7 +14,7 @@ export default async function Post ({ params }) {
   const { id } = params
   const comments = await fetchCommnets(id)
   return (
-    <ul style={{ background: '#444', fontSize: 'minmmax(12px, 16px)' }}>
+    <ul style={{ background: '#D0E3CB', fontSize: 'minmmax(12px, 16px)', listStyleType: 'none' }}>
       {comments.map(comment => (
         <li key={comment.id}>
           <h4>{comment.name}</h4>
